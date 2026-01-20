fe7a0816ec7b47d4a6f98f7a5cb677d510f64a31
andriy-titov-coder <andreytitov19880811@gmail.com>
2026-01-20 14:27:52 +0200

    Refactor: move configuration logic to a separate config.py file

--- discord_quest_bot/main.py
+++ discord_quest_bot/main.py
@@ -1,12 +1,8 @@
 import discord
 from discord.ext import commands
 import asyncio
-import os
-from dotenv import load_dotenv
 
-# Завантажуємо змінні з .env файлу
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+from config import DISCORD_TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -118,4 +114,4 @@ async def on_ready():
     print(f'Бот {bot.user.name} запущений!')
 
 
-bot.run(TOKEN)
+bot.run(DISCORD_TOKEN)
