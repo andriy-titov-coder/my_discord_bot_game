d03a392ba3dd4d75592f0bfa871980ccb3f578a6
andriy-titov-coder <andreytitov19880811@gmail.com>
2026-01-20 14:35:31 +0200

    Refactor: move source files to src, update main.py and add requirements.txt

--- discord_quest_bot/src/main.py
+++ discord_quest_bot/src/main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 
-from config import DISCORD_TOKEN
+from discord_quest_bot.src.config import DISCORD_TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
